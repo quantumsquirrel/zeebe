20fa687c5f2b73c7c1fc995a60f885209f2d46e8
Sebastian Menski <sebastian.menski@camunda.com>
2019-02-08 10:28:16 +0100

    test(broker): set random cluster gateway ports

--- broker-core/src/test/java/io/zeebe/broker/test/EmbeddedBrokerRule.java
+++ broker-core/src/test/java/io/zeebe/broker/test/EmbeddedBrokerRule.java
@@ -58,7 +58,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Supplier;
 import org.assertj.core.util.Files;
 import org.junit.rules.ExternalResource;
