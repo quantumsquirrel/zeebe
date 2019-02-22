6568647ac85a2880240f24b5ea1633e3ea6e052a
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2019-02-22 17:13:41 +0100

    chore(broker-core): replace leader election

--- raft/src/main/java/io/zeebe/raft/controller/MemberReplicateLogController.java
+++ raft/src/main/java/io/zeebe/raft/controller/MemberReplicateLogController.java
@@ -87,11 +87,9 @@ public class MemberReplicateLogController extends Actor implements Service<Void>
     this.heartbeatInterval = raft.getConfiguration().getHeartbeatIntervalDuration();
     this.clientOutput = clientTransport.getOutput();
     this.logStream = raft.getLogStream();
-
     this.logBlockIndex = logStream.getLogBlockIndex();
     this.indexContext = logBlockIndex.createLogBlockIndexContext();
-
-    this.reader = new BufferedLogStreamReader(logStream, true);
+    this.reader = new BufferedLogStreamReader(logStream);
   }
 
   @Override
