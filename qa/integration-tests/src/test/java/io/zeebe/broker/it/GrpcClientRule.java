camunda/camunda

chore(broker-core): send NotLeaderResponse during deployment push
- instead of throwing an exception brokers will reject deployments as not
leader, which is an expected case and not an exceptional error
- ActivateJobsTest will only create a single deployment per job type
- push deployment will delay retries

qa/integration-tests/src/test/java/io/zeebe/broker/it/GrpcClientRule.java
@@ -106,18 +106,32 @@ public class GrpcClientRule extends ExternalResource {
   }
 
   public long createSingleJob(String type, Consumer<ServiceTaskBuilder> consumer, String payload) {
-    final BpmnModelInstance modelInstance =
-        Bpmn.createExecutableProcess("process")
-            .startEvent("start")
-            .serviceTask(
-                "task",
-                t -> {
-                  t.zeebeTaskType(type);
-                  consumer.accept(t);
-                })
-            .endEvent("end")
-            .done();
+    final BpmnModelInstance modelInstance = createSingleJobModelInstance(type, consumer);
+    final long workflowKey = deployWorkflow(modelInstance);
+    final long workflowInstanceKey = createWorkflowInstance(workflowKey, payload);
 
+    return RecordingExporter.jobRecords(JobIntent.CREATED)
+        .filter(j -> j.getValue().getHeaders().getWorkflowInstanceKey() == workflowInstanceKey)
+        .withType(type)
+        .getFirst()
+        .getKey();
+  }
+
+  public BpmnModelInstance createSingleJobModelInstance(
+      String jobType, Consumer<ServiceTaskBuilder> taskBuilderConsumer) {
+    return Bpmn.createExecutableProcess("process")
+        .startEvent("start")
+        .serviceTask(
+            "task",
+            t -> {
+              t.zeebeTaskType(jobType);
+              taskBuilderConsumer.accept(t);
+            })
+        .endEvent("end")
+        .done();
+  }
+
+  public long deployWorkflow(BpmnModelInstance modelInstance) {
     final DeploymentEvent deploymentEvent =
         getClient()
             .newDeployCommand()
@@ -125,22 +139,16 @@ public class GrpcClientRule extends ExternalResource {
             .send()
             .join();
     waitUntilDeploymentIsDone(deploymentEvent.getKey());
+    return deploymentEvent.getWorkflows().get(0).getWorkflowKey();
+  }
 
-    // when
-    final long workflowInstanceKey =
-        getClient()
-            .newCreateInstanceCommand()
-            .bpmnProcessId("process")
-            .latestVersion()
-            .payload(payload)
-            .send()
-            .join()
-            .getWorkflowInstanceKey();
-
-    return RecordingExporter.jobRecords(JobIntent.CREATED)
-        .filter(j -> j.getValue().getHeaders().getWorkflowInstanceKey() == workflowInstanceKey)
-        .withType(type)
-        .getFirst()
-        .getKey();
+  public long createWorkflowInstance(long workflowKey, String payload) {
+    return getClient()
+        .newCreateInstanceCommand()
+        .workflowKey(workflowKey)
+        .payload(payload)
+        .send()
+        .join()
+        .getWorkflowInstanceKey();
   }
 }

qa/integration-tests/src/test/java/io/zeebe/broker/it/job/ActivateJobsTest.java
@@ -25,6 +25,8 @@ import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.api.response.ActivateJobsResponse;
 import io.zeebe.client.api.response.ActivatedJob;
 import io.zeebe.exporter.record.Assertions;
+import io.zeebe.exporter.record.Record;
+import io.zeebe.model.bpmn.BpmnModelInstance;
 import io.zeebe.protocol.Protocol;
 import io.zeebe.protocol.intent.JobBatchIntent;
 import io.zeebe.protocol.intent.JobIntent;
@@ -212,13 +214,24 @@ public class ActivateJobsTest {
   }
 
   private List<Long> createJobs(String type, int amount, String payload) {
-    return IntStream.range(0, amount)
-        .mapToLong(i -> createJob(type, payload))
-        .boxed()
-        .collect(Collectors.toList());
-  }
+    final BpmnModelInstance modelInstance =
+        clientRule.createSingleJobModelInstance(type, b -> b.zeebeTaskHeader("foo", "bar"));
+    final long workflowKey = clientRule.deployWorkflow(modelInstance);
 
-  private long createJob(String type, String payload) {
-    return clientRule.createSingleJob(type, b -> b.zeebeTaskHeader("foo", "bar"), payload);
+    for (int i = 0; i < amount; i++) {
+      clientRule.createWorkflowInstance(workflowKey, payload);
+    }
+
+    final List<Long> jobKeys =
+        RecordingExporter.jobRecords(JobIntent.CREATED)
+            .withType(type)
+            .filter(r -> r.getValue().getHeaders().getWorkflowKey() == workflowKey)
+            .limit(amount)
+            .map(Record::getKey)
+            .collect(Collectors.toList());
+
+    assertThat(jobKeys).hasSize(amount);
+
+    return jobKeys;
   }
 }

quantumsquirrel/zeebe

qa/integration-tests/src/test/java/io/zeebe/broker/it/GrpcClientRule.java
@@ -107,18 +107,32 @@ public class GrpcClientRule extends ExternalResource {
 
   public long createSingleJob(
       String type, Consumer<ServiceTaskBuilder> consumer, String variables) {
-    final BpmnModelInstance modelInstance =
-        Bpmn.createExecutableProcess("process")
-            .startEvent("start")
-            .serviceTask(
-                "task",
-                t -> {
-                  t.zeebeTaskType(type);
-                  consumer.accept(t);
-                })
-            .endEvent("end")
-            .done();
+    final BpmnModelInstance modelInstance = createSingleJobModelInstance(type, consumer);
+    final long workflowKey = deployWorkflow(modelInstance);
+    final long workflowInstanceKey = createWorkflowInstance(workflowKey, variables);
 
+    return RecordingExporter.jobRecords(JobIntent.CREATED)
+        .withWorkflowInstanceKey(workflowInstanceKey)
+        .withType(type)
+        .getFirst()
+        .getKey();
+  }
+
+  public BpmnModelInstance createSingleJobModelInstance(
+      String jobType, Consumer<ServiceTaskBuilder> taskBuilderConsumer) {
+    return Bpmn.createExecutableProcess("process")
+        .startEvent("start")
+        .serviceTask(
+            "task",
+            t -> {
+              t.zeebeTaskType(jobType);
+              taskBuilderConsumer.accept(t);
+            })
+        .endEvent("end")
+        .done();
+  }
+
+  public long deployWorkflow(BpmnModelInstance modelInstance) {
     final DeploymentEvent deploymentEvent =
         getClient()
             .newDeployCommand()
@@ -126,22 +140,16 @@ public class GrpcClientRule extends ExternalResource {
             .send()
             .join();
     waitUntilDeploymentIsDone(deploymentEvent.getKey());
+    return deploymentEvent.getWorkflows().get(0).getWorkflowKey();
+  }
 
-    // when
-    final long workflowInstanceKey =
-        getClient()
-            .newCreateInstanceCommand()
-            .bpmnProcessId("process")
-            .latestVersion()
-            .variables(variables)
-            .send()
-            .join()
-            .getWorkflowInstanceKey();
-
-    return RecordingExporter.jobRecords(JobIntent.CREATED)
-        .withWorkflowInstanceKey(workflowInstanceKey)
-        .withType(type)
-        .getFirst()
-        .getKey();
+  public long createWorkflowInstance(long workflowKey, String variables) {
+    return getClient()
+        .newCreateInstanceCommand()
+        .workflowKey(workflowKey)
+        .variables(variables)
+        .send()
+        .join()
+        .getWorkflowInstanceKey();
   }
 }

qa/integration-tests/src/test/java/io/zeebe/broker/it/job/ActivateJobsTest.java
@@ -26,6 +26,8 @@ import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.api.response.ActivateJobsResponse;
 import io.zeebe.client.api.response.ActivatedJob;
 import io.zeebe.exporter.api.record.Assertions;
+import io.zeebe.exporter.api.record.Record;
+import io.zeebe.model.bpmn.BpmnModelInstance;
 import io.zeebe.protocol.Protocol;
 import io.zeebe.protocol.intent.JobBatchIntent;
 import io.zeebe.protocol.intent.JobIntent;
@@ -217,13 +219,24 @@ public class ActivateJobsTest {
   }
 
   private List<Long> createJobs(String type, int amount, String variables) {
-    return IntStream.range(0, amount)
-        .mapToLong(i -> createJob(type, variables))
-        .boxed()
-        .collect(Collectors.toList());
-  }
+    final BpmnModelInstance modelInstance =
+        clientRule.createSingleJobModelInstance(type, b -> b.zeebeTaskHeader("foo", "bar"));
+    final long workflowKey = clientRule.deployWorkflow(modelInstance);
 
-  private long createJob(String type, String variables) {
-    return clientRule.createSingleJob(type, b -> b.zeebeTaskHeader("foo", "bar"), variables);
+    for (int i = 0; i < amount; i++) {
+      clientRule.createWorkflowInstance(workflowKey, variables);
+    }
+
+    final List<Long> jobKeys =
+        RecordingExporter.jobRecords(JobIntent.CREATED)
+            .withType(type)
+            .filter(r -> r.getValue().getHeaders().getWorkflowKey() == workflowKey)
+            .limit(amount)
+            .map(Record::getKey)
+            .collect(Collectors.toList());
+
+    assertThat(jobKeys).hasSize(amount);
+
+    return jobKeys;
   }
 }